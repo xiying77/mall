cf945ef710087597e5b29d5e3d53a3ca1ab169b3
xiying <81@1919>
2022-07-29 16:01:28 +0800

    初始框架

---
+++ mall-coupon/src/test/java/xiying/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package xiying.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
